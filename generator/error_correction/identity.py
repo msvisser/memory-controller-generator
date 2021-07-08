8cf59e5634ddce7bf5ea4b09cc03ddbe7204ccc0
Michiel Visser <opensource@webmichiel.nl>
2021-07-08 11:30:12 +0200

    Add documentation and typing info to IdentityCode

--- generator/error_correction/identity.py
+++ generator/error_correction/identity.py
@@ -1,11 +1,21 @@
+from typing import Optional
+
 import numpy as np
+
 from . import GenericCode
 
 
 class IdentityCode(GenericCode):
-    def __init__(self, data_bits):
+    """
+    Implementation of the identity code
+
+    This error correction code does not actually do any error detection or correction. Instead, it will simply pass
+    the input data to the encoded output. When decoding, there is no check to verify the correctness of the data.
+    """
+
+    def __init__(self, data_bits: int):
         super().__init__(data_bits=data_bits, parity_bits=0)
 
-    def generate_matrices(self, timeout=None):
-        self.generator_matrix = np.identity(self.data_bits, dtype=np.uint)
-        self.parity_check_matrix = np.zeros((0, 0), dtype=np.uint)
+    def generate_matrices(self, timeout: Optional[float] = None):
+        self.generator_matrix = np.identity(self.data_bits, dtype=np.int)
+        self.parity_check_matrix = np.zeros((0, 0), dtype=np.int)
