f7de535d055cf5bd08becc2566efe78ba2bbc6d4
Michiel Visser <opensource@webmichiel.nl>
2021-07-13 13:08:38 +0200

    Expose all Generic modules

--- generator/error_correction/__init__.py
+++ generator/error_correction/__init__.py
@@ -1,5 +1,5 @@
 # Re-export the different error correction codes for easier use
-from .generic import GenericCode
+from .generic import GenericCode, GenericEncoder, GenericDecoder, GenericFlipCalculator, GenericErrorCalculator
 from .identity import IdentityCode
 from .parity import ParityCode
 from .hamming import HammingCode, ExtendedHammingCode
