e0d2a65dee6a2692267bd62e046b6d2473e806df
Michiel Visser <opensource@webmichiel.nl>
2021-07-21 16:10:11 +0200

    Add commandline arguments to main.py
    Add commandline arguments to select the number of data bits and the
    error correction code from the commandline. Also output the time it took
    to generate the matrices for the code.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-import numpy as np
-from nmigen import *
-from nmigen.cli import main
-from nmigen.asserts import Assert
-
+import logging
+import time
 from functools import reduce
 
-from generator.error_correction import HsiaoCode, ExtendedHammingCode, HammingCode, ParityCode, IdentityCode
+import numpy as np
+from nmigen import *
+from nmigen.asserts import Assert
+from nmigen.cli import main_parser, main_runner
+
+import generator.error_correction
 
 
 class TestTop(Elaboratable):
-    def __init__(self, data_bits):
+    def __init__(self, data_bits, code_name):
         self.data_bits = data_bits
-        self.code = HsiaoCode(data_bits=data_bits)
+
+        # Dynamically select the error correction code based on the supplied name
+        if not hasattr(generator.error_correction, code_name):
+            raise ValueError(f"Unknown error correction code: {code_name}")
+        code_class = getattr(generator.error_correction, code_name)
+        self.code = code_class(data_bits=data_bits)
 
         self.write_data = Signal(unsigned(self.code.total_bits))
         self.read_data = Signal(unsigned(self.code.total_bits))
@@ -25,7 +32,16 @@ class TestTop(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
+        # Measure the time it takes to generate the matrices for this code
+        start = time.time()
         self.code.generate_matrices(timeout=30.0)
+        duration = 1000 * (time.time() - start)
+        logging.info(f"Matrix generation took {duration:.2f}ms")
+
+        # Log the parity-check matrix
+        logging.debug("Parity-check matrix:")
+        for row in self.code.parity_check_matrix:
+            logging.debug(f"  {row}")
 
         m.submodules.encoder = encoder = self.code.encoder()
         m.submodules.decoder = decoder = self.code.decoder()
@@ -86,9 +102,29 @@ class TestTop(Elaboratable):
 
 if __name__ == "__main__":
     np.set_printoptions(linewidth=200)
-    top = TestTop(data_bits=32)
+
+    # Build the commandline argument parser
+    parser = main_parser()
+    parser.add_argument("-c", "--code", dest="code_name", default="HammingCode")
+    parser.add_argument("-b", "--bits", dest="data_bits", default=32, type=int)
+    parser.add_argument("-v", "--verbose", dest="verbose", action="count", default=0)
+    args = parser.parse_args()
+
+    # Set the logging level based on the verbose-ness
+    if args.verbose == 0:
+        log_level = logging.INFO
+    else:
+        log_level = logging.DEBUG
+    log_format = "%(levelname)8s: %(message)s"
+    logging.basicConfig(level=log_level, format=log_format)
+
+    # Create top module
+    logging.info(f"Selected code: {args.code_name}, with {args.data_bits} bits")
+    top = TestTop(data_bits=args.data_bits, code_name=args.code_name)
+
+    # Set the platform
     platform = None
     # platform = "formal"
-    main(design=top, platform=platform, name="top", ports=top.ports())
 
-
+    # Run the nMigen main runner
+    main_runner(parser, args, design=top, platform=platform, name="top", ports=top.ports())
