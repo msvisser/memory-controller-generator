4e3b5b45e3c624eea0e3743d5d34d2dc62137335
Michiel Visser <opensource@webmichiel.nl>
2021-12-16 20:49:27 +0100

    Add documentation to RefreshController

--- generator/controller/refresh.py
+++ generator/controller/refresh.py
@@ -1,17 +1,28 @@
 from nmigen import *
 
-from .write_back import WriteBackController
-from .refresh_wrapper import RefreshWrapper
 from .generic import GenericController
+from .refresh_wrapper import RefreshWrapper
+from .write_back import WriteBackController
 
 
 class RefreshController(GenericController):
+    """
+    Implementation of an automatically refreshing memory controller.
+
+    This implementation combines the `WriteBackController` with a `RefreshWrapper` to produce a controller which will
+    periodically refresh memory locations.
+
+    For details on the implementation of the refresh mechanism see the `RefreshWrapper` implementation.
+    """
+
     def elaborate(self, platform):
         m = Module()
 
+        # Create `RefreshWrapper` and `WriteBackController`
         m.submodules.refresh = refresh = RefreshWrapper(self.addr_width, self.code.data_bits, refresh_counter_width=7)
         m.submodules.controller = controller = WriteBackController(self.code, self.addr_width)
 
+        # Connect the refresh wrapper and controller to the external signals
         m.d.comb += [
             self.req.connect(refresh.req_in),
             refresh.req_out.connect(controller.req),
