5c0c14885405e15375b8870546b53a6d7207211b
Michiel Visser <opensource@webmichiel.nl>
2021-12-16 20:38:45 +0100

    Add configuration options to RefreshWrapper

--- generator/controller/refresh_wrapper.py
+++ generator/controller/refresh_wrapper.py
@@ -16,11 +16,29 @@ class RefreshWrapper(Elaboratable):
 
     To simplify the use of this wrapper, a controller implementation `RefreshController` exists which combines the
     `WriteBackController` with this `RefreshWrapper` to create a complete controller.
+
+    By setting `force_refresh` the controller will forcefully do a refresh when the counter rolls over. Otherwise it
+    will wait for the first cycle where there is no incomming request. While this is generally nicer, since it tries
+    to avoid contention on the bus. However, in some cases the bus might be so busy that there is no time for refresh
+    operations. In that case you might want to enable `force_refresh`.
+
+    The options `address_and`, `address_or` and `address_sext` allow for manipulations of the generated refresh
+    address. The address counter will first be and-ed with `address_and`, then or-ed with `address_or`. Finally the
+    `address_sext` option allows for sign extending a part of the generated address. All one bits in the
+    `address_sext` option result in replacing that address bit with the highest non set bit. By setting the upper `n`
+    bits, the refresh address will only target a low and a high part of the memory. The combination of these options
+    allows for some flexibility in which areas of the memory are actually refreshed.
     """
-    def __init__(self, addr_width: int, data_bits: int, refresh_counter_width: int):
+
+    def __init__(self, addr_width: int, data_bits: int, refresh_counter_width: int, force_refresh=False, address_and=-1,
+                 address_or=0, address_sext=0):
         self.addr_width = addr_width
         self.data_bits = data_bits
         self.refresh_counter_width = refresh_counter_width
+        self.force_refresh = force_refresh
+        self.address_and = address_and
+        self.address_or = address_or
+        self.address_sext = address_sext
 
         self.req_in = MemoryRequestRecord(addr_width, data_bits)
         self.rsp_out = MemoryResponseRecord(data_bits)
@@ -53,12 +71,21 @@ class RefreshWrapper(Elaboratable):
         waiting_for_response = Signal()
 
         with m.If(~waiting_for_response):
-            with m.If(refresh_pending):
-                m.d.comb += [
-                    self.req_in.ready.eq(0),
+            with m.If(refresh_pending & (self.force_refresh | ~self.req_in.valid)):
+                if self.force_refresh:
+                    # Block any incomming requests
+                    m.d.comb += self.req_in.ready.eq(0)
 
+                # Apply a read request with the refresh address
+                calc_address = Signal(unsigned(self.addr_width))
+                m.d.comb += calc_address.eq((current_address & self.address_and) | self.address_or)
+                for i in range(self.addr_width):
+                    with m.If(self.address_sext & (1 << i)):
+                        m.d.comb += calc_address[i].eq(calc_address[i - 1])
+
+                m.d.comb += [
                     self.req_out.valid.eq(1),
-                    self.req_out.addr.eq(current_address),
+                    self.req_out.addr.eq(calc_address),
                     self.req_out.write_en.eq(0),
                 ]
 
